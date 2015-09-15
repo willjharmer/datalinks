daca76131f19506f3870c8880e594b3533fbe9b0
William Harmer <willjharmer@gmail.com>
2015-09-16 00:28:39 +0100

    added name migration to user table and updated forms and submitted by

--- app/views/comments/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @comment, :id, :link_id, :body, :user_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915224336) do
+ActiveRecord::Schema.define(version: 20150915230504) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20150915224336) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
